78b03ebb5cbc7dac9ff389fa6e6fa98a3a7cfc62
araimov06 <raimovadhamjon@gmail.com>
2024-09-19 18:23:38 +0500

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long int n;
+    cin >> n;
+    if (n>= -2147483647 && n<=2147483647){
+        cout << "YES";
+    }else{
+        cout <<"NO";
+    }
+
+    return 0;
+}
